75c874a97c9b284e28c4440aa7ac9045df110edd
MapleShao <939078792@qq.com>
2017-03-20 17:28:55 +0800

    update RenYe uint test

--- app/src/androidTest/java/com/maple/quce/renye/RenYeActivityTest.java
+++ app/src/androidTest/java/com/maple/quce/renye/RenYeActivityTest.java
@@ -1,8 +1,5 @@
 package com.maple.quce.renye;
 
-import android.app.Activity;
-import android.app.Instrumentation;
-import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -38,15 +35,13 @@ public class RenYeActivityTest {
 
     @Test
     public void verifyTest() {
-        // onView(ViewMatchers.withId(R.id.edt_username)).perform(typeText("ming1"), closeSoftKeyboard());
-        // onView(withId(R.id.tv_title)).perform(replaceText("Mmmmmmm9"));
-//         onView(withId(R.id.rg_select)).perform(isNotChecked());
 
         onView(withText("人业简介")).check(matches(isDisplayed()));
         onView(withId(R.id.tv_start_test)).perform(click());
 
+        sleep(1000);
+
         onView(withText("第1题")).check(matches(isDisplayed()));
-        onView(withId(R.id.bt_next)).check(matches(withText("第1题")));
         onView(withText("YES")).perform(click());
         onView(allOf(withId(R.id.bt_next), withText("下一题"))).perform(click());
 
@@ -62,12 +57,9 @@ public class RenYeActivityTest {
         onView(withText("YES")).perform(click());
         onView(withId(R.id.bt_next)).perform(click());
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        onView(withId(R.id.bt_next)).perform(click());
+        sleep(3000);
+
+//        onView(withId(R.id.bt_next)).perform(click());
 
         // onView(withText("人业简介")).check(doesNotExist());
 
@@ -76,22 +68,13 @@ public class RenYeActivityTest {
 
     }
 
-    @Test
-    public void validateIntentSentToPackage() {
-        // Build a result to return when a particular activity is launched.
-        Intent resultData = new Intent();
-        String phoneNumber = "123-345-6789";
-        resultData.putExtra("phone", phoneNumber);
-        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
-
-        // Set up result stubbing when an intent sent to "contacts" is seen.
-        // intending(toPackage("com.android.contacts")).respondWith(result);
-
-        // User action that results in "contacts" activity being launched.
-        // Launching activity expects phoneNumber to be returned and displays it on the screen.
-        // onView(withId(R.id.pickButton)).perform(click());
-
-        // Assert that data we set up above is shown.
-        // onView(withId(R.id.phoneNumber).check(matches(withText(phoneNumber)));
+    private void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
